8bee4aef191c22b9e2c92810b6aab01ac7412405
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-12-23 19:40:13 +0100

    Display key and values for rhymes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const rhymes = {
+  'La nutria come pescado...': '... y atraviesa el río a nado.',
+  'Ruge estático el león...': '... feroz, aunque mayestático.',
+  'Más que a muchos señorones...': '... gusta un libro a los ratones.',
+  'Presume cuando va llena...': '... del surtidor la ballena.'
+};
+
 ReactDOM.render(
-  <App />,
+  <App rhymes={rhymes} />,
   document.getElementById('root')
 );

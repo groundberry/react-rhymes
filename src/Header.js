37bc30d1fe952e6af8874633148e3e20309559b9
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-12-24 12:10:50 +0100

    Remove unnecessary constructor in Header.

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  constructor() {
-    super();
-  }
-
   render() {
     return (
       <h1 className="Header">Christmas quiz</h1>

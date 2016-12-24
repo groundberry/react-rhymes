2cee8fa216ec6ce370d21f5e28ea9c5a30113230
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-12-24 12:22:36 +0100

    Add header and footer (#2)

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <h3 className="Footer">© Blanca Mendizabal Perello 2016</h3>
+    );
+  }
+}
+
+export default Footer;

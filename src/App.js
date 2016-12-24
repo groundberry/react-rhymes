9ea3db7c8076318cc4faee49a3f6249a2ecc78f9
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-12-24 11:41:40 +0100

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 import './App.css';
 
@@ -29,6 +30,7 @@ class App extends Component {
             <li>{value}</li>
           )}
         </ul>
+        <Footer />
       </div>
     );
   }

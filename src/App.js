a5271f34e41e655f2de517174102ae42244c59e7
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-12-25 18:28:57 +0100

    Shuffle sentences

--- src/App.js
+++ src/App.js
@@ -12,10 +12,21 @@ class App extends Component {
     const keys = Object.keys(props.rhymes);
     const index = Math.floor(Math.random() * keys.length);
     const firstHalf = keys[index];
+    const secondHalves = Object.values(props.rhymes);
+
+    const shuffleArray = (array) => {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+      return array;
+    }
 
     this.state = {
       firstHalf: firstHalf,
-      secondHalves: Object.values(props.rhymes),
+      secondHalves: shuffleArray(secondHalves),
       selection: null,
       correct: false
     };

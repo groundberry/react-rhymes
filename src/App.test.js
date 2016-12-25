225684f1c26fe9e18f01839300ceb92863b3e6b4
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-12-25 18:23:14 +0100

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const rhymes = {
+    a: 'a',
+    b: 'b'
+  };
+  ReactDOM.render(
+    <App rhymes={rhymes}/>,
+    document.createElement('div')
+  );
 });
